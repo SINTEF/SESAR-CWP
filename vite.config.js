083907bca1bd741c6da8aa2b972d020b70f09693
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2022-07-05 13:04:45 +0200

    🧹🔥 Cleaning v2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
   plugins: [react(), commonjs()],
+  build: {
+    minify: true,
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       mqtt: 'mqtt/dist/mqtt.js',
