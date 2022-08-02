1d02dd598cf16ac62813b8bdd5763e8b5d1dbfce
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2022-08-02 14:08:37 +0200

    ⬆️ Upgrade to vite 3

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-  plugins: [react(), commonjs()],
+  plugins: [react()],
   build: {
     minify: true,
     sourcemap: true,
+    commonjsOptions: {
+      include: [],
+    },
+    target: ['es2020'],
   },
   resolve: {
     alias: {
       mqtt: 'mqtt/dist/mqtt.js',
     },
+  },
+  optimizeDeps: {
+    disabled: false,
   },
 });
