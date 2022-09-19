db90524a9dc974ddd23e705c5e15fe71428725cc
Antoine Pultier <45740+fungiboletus@users.noreply.github.com>
2022-09-19 11:08:04 +0200

    🔧 Make the application more configurable

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,9 +2,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_MAPBOX_ACCESS_TOKEN: string | undefined;
   readonly VITE_VOICE_SERVER_ENDPOINT: string | undefined;
   readonly VITE_VOICE_SERVER_BEARER_TOKEN: string | undefined;
+  readonly VITE_MQTT_BROKER_URL: string | undefined;
+  readonly VITE_BUILDTIME: string | undefined;
+  readonly VITE_VERSION: string | undefined;
+  readonly VITE_REVISION: string | undefined;
 }
 
 interface ImportMeta {
